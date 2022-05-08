4618ab94546de4d7bed2f76f78772ee821240e25
wenjin1997 <wenjin1997@126.com>
2022-05-08 16:02:14 +0800

    项目一：发现负面新闻

---
+++ news/src/main/java/com/jinjin/news/controller/NegativeNews.java
@@ -0,0 +1,33 @@
+package com.jinjin.news.controller;
+
+/**
+ * @author 文进
+ * @version 1.0
+ */
+public class NegativeNews {
+    private String title;
+    private String url;
+
+
+    public NegativeNews(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
